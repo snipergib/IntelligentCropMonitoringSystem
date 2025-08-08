30e96167efe39fb4bc5d3b2ecced702acfc39074
Earnest S <92523732+snipergib@users.noreply.github.com>
2025-08-08 13:42:59 +0530

    database.py

--- database.py
+++ database.py
@@ -1,17 +1,18 @@
 from sqlalchemy import create_engine
 
 # 🔐 Direct credentials (Supabase PostgreSQL connection)
-DB_USER = "postgres"
-DB_PASS = "759243168"
-DB_HOST = "db.ohoxmyshkdvctgilxdca.supabase.co"
-DB_PORT = "5432"
+DB_USER = "USERNAME"
+DB_PASS = "PASS"
+DB_HOST = "SUPABASE-LINK"
+DB_PORT = "PORT-NO"
 DB_NAME = "postgres"
 
 # ✅ Full connection URL (with SSL required by Supabase)
-DATABASE_URL = f"postgresql://postgres:759243168@db.ohoxmyshkdvctgilxdca.supabase.co:5432/postgres?sslmode=require"
+DATABASE_URL = "DATABASE-URL"
 
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
 
 def get_engine():
+
     return engine
